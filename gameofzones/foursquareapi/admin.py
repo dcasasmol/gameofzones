4dec0628f5055014473d64aeb15aba3103fcbdaa
David Casas Molina <david.casasmolina@gmail.com>
2013-03-21 19:44:13 +0100

    django admin site activate

---
+++ gameofzones/foursquareapi/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from foursquareapi.models import Categorie, User, Zone, Venue, Checkin
+
+admin.site.register(Categorie)
+admin.site.register(User)
+admin.site.register(Zone)
+admin.site.register(Venue)
+admin.site.register(Checkin)
