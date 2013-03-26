496897446e83feebd2179b2e0c6b213ea933c06e
David Casas Molina <david.casasmolina@gmail.com>
2013-03-26 19:15:02 +0100

    old app deleted

--- gameofzones/foursquareapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from foursquareapi.models import Categorie, User, Zone, Venue, Checkin
-
-admin.site.register(Categorie)
-admin.site.register(User)
-admin.site.register(Zone)
-admin.site.register(Venue)
-admin.site.register(Checkin)
